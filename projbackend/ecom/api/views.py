2dbcbf50dacc1ac0d0c3112371b75df1552541c9
Rithesh-Rithu <ritheshdavid@gmail.com>
2020-08-11 22:34:47 +0530

    Backend

---
+++ projbackend/ecom/api/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+
+# Create your views here.
+
+def home(request):
+    return JsonResponse({
+        'info': "Django react",
+        'name': "Rithesh"
+    })
